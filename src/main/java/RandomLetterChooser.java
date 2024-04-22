8e4dd402ecbec3dac46f0bb8dfb7d1ce087a1ff3
TrevorAPCS <142834649+TrevorAPCS@users.noreply.github.com>
2024-04-22 12:11:25 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -2,7 +2,7 @@ public class RandomLetterChooser extends RandomStringChooser
 {
 	private String[] letters;
 	public RandomLetterChooser(String str)
-	{ 
+	{
 		letterArray = new String[str.length()];
 		for(int i = 0; i < str.length(); i++){
 			letters.add(str.substing(i, i + 1));
